7cc3115e8669568f6fd2e83b9735e2dad3c5099f
Pål Vårdal Gjerde <paalvaa@gmail.com>
2021-12-27 01:36:42 +0100

    When casting Survey, the minimap icons are now colored based on their distance from the survey. Three new buttons have been added to the mini-window to filter out icons based on the survey results.

--- MinimapManager.lua
+++ MinimapManager.lua
@@ -6,32 +6,91 @@ local MinimapManager = Class:extend()
 
 function MinimapManager:construct()
     self.hbdPins = LibStub('HereBeDragons-Pins-2.0')
+    self.surveys = {}
+    self.lastDigsite = 0
 
-    Excavatinator.events.enterDigsite:addListener(function()
-        local finds = Excavatinator:getFindsForCurrentDigsite()
-        for i, find in ipairs(finds) do
-            self:createPinForFind(find)
-        end
+    Excavatinator.events.enterDigsite:addListener(function(digsite)
+        if digsite.id ~= self.lastDigsite then self.surveys = {} end
+        self.lastDigsite = digsite.id
+        self:updatePins()
     end)
 
     Excavatinator.events.leaveDigsite:addListener(function()
-        self.hbdPins:RemoveAllMinimapIcons('Excavatinator')
+        self:updatePins()
     end)
 
     Excavatinator.events.newDigsiteFind:addListener(function(find)
-        self:createPinForFind(find)
+        self:updatePins()
+    end)
+
+    private.WoWEvents.ARCHAEOLOGY_SURVEY_CAST:addListener(function(finds, totalFinds, raceID, success)
+        if success == 1 then
+            self.surveys = {}
+        else
+            local playerX, playerY = UnitPosition('player')
+            self.surveys[#self.surveys+1] = {
+                x = playerX,
+                y = playerY,
+                result = nil,
+            }
+        end
+        self:updatePins()
     end)
 end
 
+function MinimapManager:updatePins()
+    self.hbdPins:RemoveAllMinimapIcons('Excavatinator')
+    local finds = Excavatinator:getFindsForCurrentDigsite()
+    for i, find in ipairs(finds) do
+        self:createPinForFind(find)
+    end
+end
+
 function MinimapManager:createPinForFind(find)
+    local possibleNext = true
+    for i = 1, #self.surveys do
+        if self.surveys[i].result ~= nil then
+            local distance = CalculateDistance(self.surveys[i].x, self.surveys[i].y, find.worldX, find.worldY)
+            if (self.surveys[i].result == 'green' and distance > 45)
+                or (self.surveys[i].result == 'yellow' and distance < 35)
+                or (self.surveys[i].result == 'yellow' and distance > 85)
+                or (self.surveys[i].result == 'red' and distance < 75)
+            then
+                possibleNext = false
+                break
+            end
+        end
+    end
+
+    local distance = -1
+
+    if #self.surveys > 0 then
+        distance = CalculateDistance(self.surveys[#self.surveys].x, self.surveys[#self.surveys].y, find.worldX, find.worldY)
+    end
+
     local icon = CreateFrame('Frame')
     icon:SetWidth(10)
     icon:SetHeight(10)
     icon.texture = icon:CreateTexture()
     icon.texture:SetTexture(457644)
     icon.texture:SetDrawLayer("BACKGROUND")
+    if not possibleNext then icon.texture:SetAlpha(0.3) end
+    if distance ~= -1 then
+        if distance <= 40 then icon.texture:SetVertexColor(0, 255, 0) end
+        if distance > 40 and distance <= 80 then icon.texture:SetVertexColor(255, 255, 0) end
+        if distance > 80 then icon.texture:SetVertexColor(255, 0, 0) end
+    end
     icon.texture:SetAllPoints()
     self.hbdPins:AddMinimapIconMap('Excavatinator', icon, find.mapID, find.mapX, find.mapY, true)
+end
+
+function MinimapManager:setLastSurveyResult(result)
+    -- Result is one of "red", "yellow", "green"
+    if #self.surveys == 0 then return end
+
+    self.surveys[#self.surveys].result = result
+
+    self:updatePins()
 end
 
 private.loadMinimapManager = function()
